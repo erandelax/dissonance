0c164cb78560aaca5dd123c28909a65241f34615
Erandelax <erandelax@gmail.com>
2021-10-23 23:33:18 +0300

    sync

--- resources/views/profiles/profile_list.blade.php
+++ resources/views/profiles/profile_list.blade.php
@@ -10,7 +10,6 @@
     </div>
     <div class="container" style="display:flex;flex-direction: row;flex-wrap: wrap;">
         @foreach ($users as $user)
-            @for ($pos = 0; $pos < 2; $pos++)
                 <a href="{{route('profile',['discordID' => $user->discord_id, 'locale' => app()->getLocale()])}}"
                    class="card sponsor-section-card w-350 mw-full m-10 p-0 d-flex" rel="noopener">
                     <div class="w-100 h-100 m-10 align-self-center">
@@ -35,36 +34,6 @@
                         <div class="sponsor-section-card-scroll-block"></div>
                     </div>
                 </a>
-            @endfor
         @endforeach
     </div>
-    {{--<div class="layout-center">
-        <div class="layout-page">
-            <h1>Users</h1>
-            <table style="width:100%;border:1px solid white;border-collapse:collapse">
-                <tr>
-                @foreach ($users as $user)
-                    <td>
-                        {{$user->discord_id}}
-                    </td>
-                    <td style="border:1px solid white">
-                        <img class="user-avatar" src="{{$user->avatar}}"/>
-                    </td>
-                    <td style="border:1px solid white">
-                        <a class="menu-item" href="{{route('profile',['discordID' => $user->discord_id, 'locale' => app()->getLocale()])}}">
-                            {{$user->name}}
-                        </a>
-                    </td>
-                    <td style="border:1px solid white">
-                        {{$user->created_at}}
-                    </td>
-                @endforeach
-                </tr>
-            </table>
-            <div>
-                {!! $users->render() !!}
-            </div>
-        </div>
-    </div>
-    --}}
 @endsection
